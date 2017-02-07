b480250ff86cb2258d15ab352badcdf398606071
emersion <contact@emersion.fr>
2017-02-07 16:13:26 +0100

    Fixes RFC test

--- verify_test.go
+++ verify_test.go
@@ -3,24 +3,20 @@ package dkim
 import (
 	"strings"
 	"testing"
+	"reflect"
 )
 
-const verifiedMailString = `X-Authentication-Results: shopping.example.net
-  header.from=joe@football.example.com; dkim=pass
-Received: from mout23.football.example.com (192.168.1.1)
-  by shopping.example.net with SMTP;
-  Fri, 11 Jul 2003 21:01:59 -0700 (PDT)
-DKIM-Signature: v=1; a=rsa-sha256; s=brisbane; d=example.com;
-  c=simple/simple; q=dns/txt; i=joe@football.example.com;
-  h=Received : From : To : Subject : Date : Message-ID;
-  bh=2jUSOH9NhtVGCQWNr9BrIAPreKQjO6Sn7XIkfJVOzv8=;
-  b=AuUoFEfDxTDkHlLXSZEpZj79LICEps6eda7W3deTVFOk4yAUoqOB
-    4nujc7YopdG5dWLSdNg6xNAZpOPr+kHxt1IrE+NahM6L/LbvaHut
-    KVdkLLkpVaVVQPzeRDI009SO2Il5Lu7rDNH6mZckBdrIx0orEtZV
-    4bmp/YzhwvcubU4=;
+const verifiedMailString = `DKIM-Signature: v=1; a=rsa-sha256; s=brisbane; d=example.com;
+      c=simple/simple; q=dns/txt; i=joe@football.example.com;
+      h=Received : From : To : Subject : Date : Message-ID;
+      bh=2jUSOH9NhtVGCQWNr9BrIAPreKQjO6Sn7XIkfJVOzv8=;
+      b=AuUoFEfDxTDkHlLXSZEpZj79LICEps6eda7W3deTVFOk4yAUoqOB
+      4nujc7YopdG5dWLSdNg6xNAZpOPr+kHxt1IrE+NahM6L/LbvaHut
+      KVdkLLkpVaVVQPzeRDI009SO2Il5Lu7rDNH6mZckBdrIx0orEtZV
+      4bmp/YzhwvcubU4=;
 Received: from client1.football.example.com  [192.0.2.1]
-  by submitserver.example.com with SUBMISSION;
-  Fri, 11 Jul 2003 21:01:54 -0700 (PDT)
+      by submitserver.example.com with SUBMISSION;
+      Fri, 11 Jul 2003 21:01:54 -0700 (PDT)
 From: Joe SixPack <joe@football.example.com>
 To: Suzie Q <suzie@shopping.example.net>
 Subject: Is dinner ready?
@@ -34,12 +30,25 @@ We lost the game. Are you hungry yet?
 Joe.
 `
 
-// Don't know why this doesn't work...
+var testVerification = &Verification{
+	Domain: "example.com",
+	Identifier: "joe@football.example.com",
+	HeaderKeys: []string{"Received", "From", "To", "Subject", "Date", "Message-ID"},
+	BodyLength: -1,
+}
+
 func TestVerify(t *testing.T) {
 	r := strings.NewReader(strings.Replace(verifiedMailString, "\n", "\r\n", -1))
 
-	_, err := Verify(r)
+	verifications, err := Verify(r)
 	if err != nil {
-		//t.Errorf("Expected no error while verifying signature, got: %v", err)
+		t.Fatalf("Expected no error while verifying signature, got: %v", err)
+	} else if len(verifications) != 1 {
+		t.Fatalf("Expected exactly one verification, got %v", len(verifications))
+	}
+
+	v := verifications[0]
+	if !reflect.DeepEqual(testVerification, v) {
+		t.Errorf("Expected verification to be \n%+v\n but got \n%+v", testVerification, v)
 	}
 }
