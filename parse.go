ffa47165b5bd1eeb1054ad8d28e2d26a891fa136
emersion <contact@emersion.fr>
2017-02-23 22:36:11 +0100

    Adds parsing

---
+++ parse.go
@@ -0,0 +1,148 @@
+package msgauth
+
+import (
+	"errors"
+	"strings"
+)
+
+type ResultValue string
+
+// Authentication result values, as defined in RFC 5451 section 6.3.
+const (
+	ResultNone ResultValue = "none"
+	ResultPass = "pass"
+	ResultFail = "fail"
+	ResultPolicy = "policy"
+	ResultNeutral = "neutral"
+	ResultTempError = "temperror"
+	ResultPermError = "permerror"
+	ResultHardFail = "hardfail"
+	ResultSoftFail = "softfail"
+)
+
+type Result interface {}
+
+type AuthResult struct {
+	Value ResultValue
+	Auth string
+}
+
+type DKIMResult struct {
+	Value ResultValue
+	Domain string
+	Identifier string
+}
+
+type IPRevResult struct {
+	Value ResultValue
+	IP string
+}
+
+type SenderIDResult struct {
+	Value ResultValue
+	HeaderKey string
+	HeaderValue string
+}
+
+type SPFResult struct {
+	Value ResultValue
+	From string
+	Helo string
+}
+
+type unknownResult struct {
+	Value ResultValue
+	Params map[string]string
+}
+
+type newResultFunc func(v ResultValue, params map[string]string) Result
+
+var results = map[string]newResultFunc{
+	"auth": func(v ResultValue, params map[string]string) Result {
+		return &AuthResult{Value: v, Auth: params["smtp.auth"]}
+	},
+	"dkim": func(v ResultValue, params map[string]string) Result {
+		return &DKIMResult{Value: v, Domain: params["header.d"], Identifier: params["header.i"]}
+	},
+	"domainkeys": nil, // TODO
+	"iprev": func(v ResultValue, params map[string]string) Result {
+		return &IPRevResult{Value: v, IP: params["policy.iprev"]}
+	},
+	"sender-id": func(v ResultValue, params map[string]string) Result {
+		result := &SenderIDResult{Value: v}
+		for k, v := range params {
+			if strings.HasPrefix(k, "header.") {
+				result.HeaderKey = strings.TrimPrefix(k, "header.")
+				result.HeaderValue = v
+				break
+			}
+		}
+		return result
+	},
+	"spf": func(v ResultValue, params map[string]string) Result {
+		return &SPFResult{Value: v, From: params["smtp.mailfrom"], Helo: params["smtp.helo"]}
+	},
+}
+
+func parse(v string) (identifier string, results []Result, err error) {
+	parts := strings.Split(v, ";")
+	identifier = strings.TrimSpace(parts[0])
+
+	for i := 1; i < len(parts); i++ {
+		s := strings.TrimSpace(parts[i])
+		if s == "" {
+			continue
+		}
+
+		result, err := parseResult(s)
+		if err != nil {
+			return identifier, results, err
+		}
+		if result != nil {
+			results = append(results, result)
+		}
+	}
+	return
+}
+
+func parseResult(s string) (Result, error) {
+	// TODO: ignore header comments in parenthesis
+
+	// TODO: split on \t too?
+	parts := strings.Split(s, " ")
+
+	if parts[0] == "none" {
+		return nil, nil
+	}
+
+	k, v, err := parseParam(parts[0])
+	if err != nil {
+		return nil, err
+	}
+	method, value := k, ResultValue(v)
+
+	params := make(map[string]string)
+	for i := 1; i < len(parts); i++ {
+		k, v, err := parseParam(parts[i])
+		if err != nil {
+			continue
+		}
+
+		params[k] = v
+	}
+
+	newResult, ok := results[method]
+	if !ok {
+		return &unknownResult{Value: value, Params: params}, nil
+	}
+
+	return newResult(value, params), nil
+}
+
+func parseParam(s string) (k string, v string, err error) {
+	kv := strings.SplitN(s, "=", 2)
+	if len(kv) != 2 {
+		return "", "", errors.New("msgauth: malformed authentication method and value")
+	}
+	return strings.TrimSpace(kv[0]), strings.TrimSpace(kv[1]), nil
+}
---
+++ parse_test.go
@@ -0,0 +1,98 @@
+package msgauth
+
+import (
+	"reflect"
+	"testing"
+)
+
+var parseTests = []struct{
+	value string
+	identifier string
+	results []Result
+}{
+	{
+		value: "",
+		identifier: "",
+		results: nil,
+	},
+	{
+		value: "example.org; none",
+		identifier: "example.org",
+		results: nil,
+	},
+	{
+		value: "example.com;\r\n" +
+			" spf=pass smtp.mailfrom=example.net",
+		identifier: "example.com",
+		results: []Result{
+			&SPFResult{Value: ResultPass, From: "example.net"},
+		},
+	},
+	{
+		value: "example.com;\r\n" +
+			" auth=pass (cram-md5) smtp.auth=sender@example.com;\r\n" +
+			" spf=pass smtp.mailfrom=example.com",
+		identifier: "example.com",
+		results: []Result{
+			&AuthResult{Value: ResultPass, Auth: "sender@example.com"},
+			&SPFResult{Value: ResultPass, From: "example.com"},
+		},
+	},
+	{
+		value: "example.com;\r\n" +
+			" sender-id=pass header.from=example.com",
+		identifier: "example.com",
+		results: []Result{
+			&SenderIDResult{Value: ResultPass, HeaderKey: "from", HeaderValue: "example.com"},
+		},
+	},
+	{
+		value: "example.com;\r\n" +
+			" sender-id=hardfail header.from=example.com;\r\n" +
+			" dkim=pass (good signature) header.i=sender@example.com",
+		identifier: "example.com",
+		results: []Result{
+			&SenderIDResult{Value: ResultHardFail, HeaderKey: "from", HeaderValue: "example.com"},
+			&DKIMResult{Value: ResultPass, Identifier: "sender@example.com"},
+		},
+	},
+	{
+		value: "example.com;\r\n" +
+			" auth=pass (cram-md5) smtp.auth=sender@example.com;\r\n" +
+			" spf=hardfail smtp.mailfrom=example.com",
+		identifier: "example.com",
+		results: []Result{
+			&AuthResult{Value: ResultPass, Auth: "sender@example.com"},
+			&SPFResult{Value: ResultHardFail, From: "example.com"},
+		},
+	},
+	{
+		value: "example.com;\r\n" +
+			" dkim=pass (good signature) header.i=@mail-router.example.net;\r\n" +
+			" dkim=fail (bad signature) header.i=@newyork.example.com",
+		identifier: "example.com",
+		results: []Result{
+			&DKIMResult{Value: ResultPass, Identifier: "@mail-router.example.net"},
+			&DKIMResult{Value: ResultFail, Identifier: "@newyork.example.com"},
+		},
+	},
+}
+
+func TestParse(t *testing.T) {
+	for _, test := range parseTests {
+		identifier, results, err := parse(test.value)
+		if err != nil {
+			t.Errorf("Excpected no error when parsing header, got: %v", err)
+		} else if test.identifier != identifier {
+			t.Errorf("Expected identifier to be %q, but got %q", test.identifier, identifier)
+		} else if len(test.results) != len(results) {
+			t.Errorf("Expected number of results to be %v, but got %v", len(test.results), len(results))
+		} else {
+			for i := 0; i < len(results); i++ {
+				if !reflect.DeepEqual(test.results[i], results[i]) {
+					t.Errorf("Expected result to be \n%v\n but got \n%v", test.results[i], results[i])
+				}
+			}
+		}
+	}
+}
