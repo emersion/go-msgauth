f82513ff1c224841ebd83fc3e7525679932e7067
emersion <contact@emersion.fr>
2019-03-22 20:38:22 +0200

    First commit

---
+++ cmd/dmarc-lookup/main.go
@@ -0,0 +1,26 @@
+//+build ignore
+
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/emersion/go-dmarc"
+)
+
+func main() {
+	flag.Parse()
+
+	domain := flag.Arg(0)
+	if domain == "" {
+		log.Fatal("usage: dmarc-lookup <domain>")
+	}
+
+	rec, err := dmarc.Lookup(domain)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("%#v\n", rec)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/emersion/go-dmarc
+
+go 1.12
