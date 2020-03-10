f9aee056c6d5aa4c606fdddd4d367157a912d8fb
Ludovico Russo <l.russo@hoplo.com>
2020-03-10 18:39:16 +0100

    Solve #23: header with extra blank line generated in in some rare cases

--- dkim/header_test.go
+++ dkim/header_test.go
@@ -102,3 +102,11 @@ func TestHeaderPicker_Pick(t *testing.T) {
 
 	})
 }
+
+func TestFoldHeaderField(t *testing.T) {
+	header := `DKIM-Signature: a=rsa-sha256; bh=bvW0aiEWdP0ie2rawBb+IiTxlHI9KgEIYjEYTGzMRa0=; c=simple/simple; d=test.xxxxxxxxxx.xxxxx; h=Return-Path:From:To:Subject:Message-ID:Date; s=brisbane; t=1583859773; v=1; b=IIlaZ79pLDdypX2YzJuy9EvxsbrqF360CLhkQqMqVC/GIa1K/X8p8POny7WuVuFTMHLvA1zH9YcXgEgIH+X0GgGsqqzq8ETL+QGZ9g1EKCmIg+ZEYV5JnH25Ar6bJpGra3jg6sIxKi6CthpK50kd8T0Q3K/oZGot4BkLaPqogpo=;`
+	folded := foldHeaderField(header)
+	if strings.HasSuffix(folded, "\r\n \n") {
+		t.Errorf("Extra black line added in header:\n ---Start--- %v ---End---", folded)
+	}
+}
