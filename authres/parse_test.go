85ee974dd2a19c38aebaf2e5d86d6044ed6cb2bb
Koni Marti <koni.marti@gmail.com>
2022-05-31 15:19:53 +0200

    authres: parser rework
    Rewrite of the Authentication-Results header parser for a complete RFC
    8601 implementation. Ignore any header comments in parenthesis. Allow
    escape sequences and semi-colons in comments and quoted strings as values.
    
    Fixes: https://github.com/emersion/go-msgauth/issues/32

--- authres/parse_test.go
+++ authres/parse_test.go
@@ -26,11 +26,84 @@ var parseTests = []msgauthTest{
 	},
 	{
 		value: "example.com;" +
+			"dkim=pass reason=\"good signature\" header.i=@mail-router.example.net;",
+		identifier: "example.com",
+		results: []Result{
+			&DKIMResult{Value: ResultPass, Reason: "good signature", Identifier: "@mail-router.example.net"},
+		},
+	},
+	{
+		value: "example.com;" +
+			"dkim=pass reason=\"good; signature\" header.i=@mail-router.example.net;",
+		identifier: "example.com",
+		results: []Result{
+			&DKIMResult{Value: ResultPass, Reason: "good; signature", Identifier: "@mail-router.example.net"},
+		},
+	},
+	{
+		value: "example.com;" +
 			" auth=pass (cram-md5) smtp.auth=sender@example.com;",
 		identifier: "example.com",
 		results: []Result{
 			&AuthResult{Value: ResultPass, Auth: "sender@example.com"},
 		},
+	},
+	{
+		value: "example.com;" +
+			" auth=pass (cram-md5) smtp.auth=sender@example.com;" +
+			" spf=pass smtp.mailfrom=example.net",
+		identifier: "example.com",
+		results: []Result{
+			&AuthResult{Value: ResultPass, Auth: "sender@example.com"},
+			&SPFResult{Value: ResultPass, From: "example.net"},
+		},
+	},
+	{
+		value: "example.com;" +
+			" auth=pass (cram-md5 (comment inside comment)) smtp.auth=sender@example.com;",
+		identifier: "example.com",
+		results: []Result{
+			&AuthResult{Value: ResultPass, Auth: "sender@example.com"},
+		},
+	},
+	{
+		value: "example.com;" +
+			" auth=pass (cram-md5; comment with semicolon) smtp.auth=sender@example.com;",
+		identifier: "example.com",
+		results: []Result{
+			&AuthResult{Value: ResultPass, Auth: "sender@example.com"},
+		},
+	},
+	{
+		value: "example.com;" +
+			" auth=pass (cram-md5 \\( comment with escaped char) smtp.auth=sender@example.com;",
+		identifier: "example.com",
+		results: []Result{
+			&AuthResult{Value: ResultPass, Auth: "sender@example.com"},
+		},
+	},
+	{
+		value: "foo.example.net (foobar) 1 (baz);" +
+			" dkim (Because I like it) / 1 (One yay) = (wait for it) fail" +
+			" policy (A dot can go here) . (like that) expired" +
+			" (this surprised me) = (as I wasn't expecting it) 1362471462",
+		identifier: "foo.example.net",
+		results: []Result{
+			&DKIMResult{Value: ResultFail, Reason: "", Domain: "", Identifier: ""},
+		},
+	},
+}
+
+var mustFailParseTests = []msgauthTest{
+	{
+		value:      " ; ",
+		identifier: "",
+		results:    nil,
+	},
+	{
+		value:      "example.com 2; none",
+		identifier: "example.com",
+		results:    nil,
 	},
 }
 
@@ -49,6 +122,12 @@ func TestParse(t *testing.T) {
 					t.Errorf("Expected result to be \n%v\n but got \n%v", test.results[i], results[i])
 				}
 			}
+		}
+	}
+	for _, test := range mustFailParseTests {
+		_, _, err := Parse(test.value)
+		if err == nil {
+			t.Errorf("Expected an error when parsing header, but got none.")
 		}
 	}
 }
